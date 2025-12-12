01bea4451feb16813e3f52ceb9e3b3099640360b
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-12-12 01:46:16 +0100

    update

--- services/Cut.js
+++ services/Cut.js
@@ -3,4 +3,15 @@ export class CutService {
         this.controller = controller;
         this.mode = 'remove';
     }
+
+    setMode(mode) {
+        console.log(mode);
+        
+        this.mode = mode;
+        
+        if (this.controller.cutterMesh) {
+            const color = mode === 'remove' ? 0xff0000 : 0x00ff00;
+            this.controller.cutterMesh.material.color.setHex(color);
+        }
+    }
 }
